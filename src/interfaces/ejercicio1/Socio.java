758ce98ed828655b2743c4a296533e1ccf2ff403
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-03-24 12:46:02 +0100

    Ejercicio 1 NO terminado.

--- src/interfaces/ejercicio1/Socio.java
+++ src/interfaces/ejercicio1/Socio.java
@@ -1,6 +1,40 @@
 package interfaces.ejercicio1;
 
-public class Socio {
+public class Socio implements Comparable<Socio>{
 
+	private int id;
+	private String nombre;
+	private int edad;
+	
+	/**
+	 * @param id
+	 * @param nombre
+	 * @param edad
+	 */
+	public Socio(int id, String nombre, int edad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + id + "\nNombre: " + nombre + "\nEdad: " + edad;
+	}
+
+	@Override
+	public int compareTo(Socio socio) {
+		int res = 0;
+		
+		if (socio.id > this.id) {
+			res = -1;
+		} else if (socio.id < this.id) {
+			res = 1;
+		}
+		
+		return res;
+	}
+	
 	
 }
