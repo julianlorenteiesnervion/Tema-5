c861aefbc04f4ecfc6356c661f860deda538b297
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-04-01 20:40:52 +0200

    Clase persona terminada.

---
+++ src/analisis/ejercicio4/Persona.java
@@ -0,0 +1,25 @@
+package analisis.ejercicio4;
+
+public class Persona extends Contacto {
+
+	private String fecha;
+
+	/**
+	 * @param nombre
+	 * @param telefono
+	 * @param fecha
+	 */
+	public Persona(String nombre, int telefono, String fecha) {
+		super(nombre, telefono);
+		this.fecha = fecha;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	
+}
