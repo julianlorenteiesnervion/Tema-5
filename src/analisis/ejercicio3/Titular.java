7019bb97fa4d8b894642297ae06cb4fa52df8d7d
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-04-02 19:12:11 +0200

    Clase Titular terminada.

--- src/analisis/ejercicio3/Titular.java
+++ src/analisis/ejercicio3/Titular.java
@@ -1,5 +1,52 @@
 package analisis.ejercicio3;
 
 public class Titular {
+	
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private int telefono;
+	
+	/**
+	 * @param dni
+	 * @param nombre
+	 * @param apellidos
+	 * @param telefono
+	 */
+	public Titular(String dni, String nombre, String apellidos, int telefono) {
+		if (dni != null && !dni.isBlank()) {
+			this.dni = dni;
+		}
+		
+		if (nombre != null && !nombre.isBlank()) {
+			this.nombre = nombre;
+		}
+		
+		if (apellidos != null && !apellidos.isBlank()) {
+			this.apellidos = apellidos;
+		}
+		
+		this.telefono = telefono;
+	}
 
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+	
 }
